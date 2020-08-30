b3405e55f98f408b4e6fd06f76ab3b3d61ca8eb6
sophieO1970 <sophieolondie@gmail.com>
2020-08-30 23:11:28 +0300

    edit index.html,javascript and styles

--- js/javascript.js
+++ js/javascript.js
@@ -1,106 +1,29 @@
-$(document).ready(function () {
-    $("#design-pic").click(function () {
-        $("#design-a").show();
-        $("#design-pic").hide();
-
-    });
-    $("#design-a").click(function () {
-        $("#design-pic").show();
-        $("#design-a").hide();
-    });
-    $("#development-pic").click(function () {
-        $("#development-a").show();
-        $("#development-pic").hide();
-
-    });
-    $("#development-a").click(function () {
-        $("#development-pic").show();
-        $("#development-a").hide();
-    });
-    $("#product-pic").click(function () {
-        $("#product-a").show();
-        $("#product-pic").hide();
-    });
-    $("#product-a").click(function () {
-        $("#product-pic").show();
-        $("#product-a").hide();
-    });
-});
-
-// $(document).ready(function(){
-//     var four= ["design", "development", "product management"]
-//     four.forEach(function (four) {
-//         $("#" + four).click(function () {
-//           $("#" + four).hide();
-//           $("#" + four + "-def").show();
-//         });
+$(document).ready(function(){
+    var services= ["design", "development", "product"]
+    services.forEach(function (service) {
+        $("#" + service).click(function () {
+          $("#" + service).hide();
+          $("#" + service+ "-a").show();
+        });
     
-//         $("#" + four + "-def").click(function () {
-//           $("#" + four).show();
-//           $("#" + four + "-def").hide();
-//         });
-//       });
-// });
+        $("#" + service + "-a").click(function () {
+          $("#" + service).show();
+          $("#" + service + "-a").hide();
+        });
+      });
+});
+for (let i = 1; i < 9; i++) {
+    $(".nad" + i).mouseover(function () {
+      $(".project" + i).show();
+    });
+    $(".nad" + i).mouseout(function () {
+      $(".project" + i).hide();
+    });
+};
 
-$(document).ready(function () {
-    $('.nad').mouseover(function () {
-        $('.project').show();
-    }).mouseout(function () {
-        $('.project').hide();
-    });
-});
-$(document).ready(function () {
-    $('.nad2').mouseover(function () {
-        $('.project2').show();
-    }).mouseout(function () {
-        $('.project2').hide();
-    });
-});
-$(document).ready(function () {
-    $('.nad3').mouseover(function () {
-        $('.project3').show();
-    }).mouseout(function () {
-        $('.project3').hide();
-    });
-});
 
-$(document).ready(function () {
-    $('.nad4').mouseover(function () {
-        $('.project4').show();
-    }).mouseout(function () {
-        $('.project4').hide();
-    });
-});
-$(document).ready(function () {
-    $('.nad5').mouseover(function () {
-        $('.project5').show();
-    }).mouseout(function () {
-        $('.project5').hide();
-    });
-});
 
-$(document).ready(function () {
-    $('.nad6').mouseover(function () {
-        $('.project6').show();
-    }).mouseout(function () {
-        $('.project6').hide();
-    });
-});
-$(document).ready(function () {
-    $('.nad7').mouseover(function () {
-        $('.project7').show();
-    }).mouseout(function () {
-        $('.project7').hide();
-    });
-});
 
-$(document).ready(function () {
-    $('.nad8').mouseover(function () {
-        $('.project8').show();
-    }).mouseout(function () {
-        $('.project8').hide();
-    });
-});
 function contact(form) {
     var name = document.forms["form2"]["name"].value;
     var email = document.forms["form2"]["email"].value;
